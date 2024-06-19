4adaa875de0f9b21cf899e7397e9db9a4b56b29d
vloax <vitords.mello@gmail.com>
2024-06-19 15:07:10 -0300

    feat: add background image and header
    Adds a starry background image to the application and refactors the header to improve readability and hierarchy.

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -8,11 +8,16 @@ export default function Header({ open, setOpen }: { open: boolean, setOpen: (val
   };
 
   return (
-    <div className="flex justify-center mt-2">
-      <button onClick={handleClick} className="flex items-center border-solid border-[1px] bg-mem rounded-xl gap-2 p-2 font-semibold m-3">
-        <PlusIcon />
-        Adicionar coluna
-      </button>
-    </div>
+    <>
+      <div className="flex justify-center mt-2 mb-[-3px]">
+        <h1 className="text-2xl font-bold">local kanban :o</h1>
+      </div>
+      <div className="flex justify-center ">
+        <button onClick={handleClick} className="flex items-center border-solid border-[1px] bg-mem rounded-xl gap-2 p-2 font-semibold m-3">
+          <PlusIcon />
+          Adicionar coluna
+        </button>
+      </div>
+    </>
   );
 }
